da8499de6dc51cafb5f5485e4fdcc3a6db3b70c2
p.ludynia <p.ludynia@godeltech.com>
2023-03-28 15:24:22 +0200

    initial commit

---
+++ src/main/java/com/example/springbootopenai/SpringBootOpenAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootopenai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOpenAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOpenAiApplication.class, args);
+    }
+
+}
